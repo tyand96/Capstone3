ae3c69fb96b9d3db32061e0eced04ff1f2ac8e18
Tyler Anderson <tyand96@gmail.com>
2023-11-30 09:24:15 +0100

    Added multer. File submission added.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
+import multer from "multer";
 import {fileURLToPath} from "url";
-import {dirname} from "path";
+import {dirname, extname} from "path";
+import {Buffer} from "buffer";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 let blogPosts = [];
@@ -16,8 +18,29 @@ class BlogPost {
     }
 }
 
+function fileFilter(req, file, callback) {
+    const ext = extname(file.originalname);
+    if (ext !== '.png' && ext !== ".jpg" && ext !== ".jpeg") {
+        return callback(new Error("Only images are allowed!"));
+    }
+    callback(null, true);
+}
+
 const app = express();
 const port = 3000;
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage, fileFilter: fileFilter});
+
+function allFieldsValid(req) {
+    console.log(req.body);
+    console.log(req.file);
+}
+
+function bufferToImgSource(imgData) {
+    const imgBuffer = imgData.buffer.toString("base64");
+    const imgSource = `data:${imgData.mimetype};base64,${imgBuffer}`;
+    return imgSource;
+}
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}));
@@ -27,6 +50,10 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+app.post("/submit", upload.single("blogImg"), (req, res) => {
+    allFieldsValid(req);
+    res.sendStatus(202);
+})
 
 
 
